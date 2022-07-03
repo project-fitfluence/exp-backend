85e5c0fad233dd103d38750d7314b68bffefc0cc
Rayco Haex <raycoh1299@gmail.com>
2022-07-03 20:23:34 +0200

    jwt documentation

--- fitfluence experimental backend/Data/ApiUser.cs
+++ fitfluence experimental backend/Data/ApiUser.cs
@@ -7,6 +7,5 @@ namespace fitfluence_experimental_backend.Data
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? ProfilePicture { get; set; }
-
     }
 }
